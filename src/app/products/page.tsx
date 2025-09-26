867db7e67e40a36f6aff9518e6377d3358728453
hamze <hamze@bodyfitteam.com.br>
2025-09-26 19:53:37 -0300

    add eslint sort plugin and prettier tailwind plugin

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,15 +1,12 @@
 import { Button } from "@/components/ui/button";
 
 const ProductPage = () => {
-
-    return (
-        <div>
-
-            <h1>Product Page</h1>
-            <Button>Primary Button</Button>
-        </div>
-    );
-
-}
+  return (
+    <div className="border border-red-50 p-5">
+      <h1>Product Page</h1>
+      <Button>Primary Button</Button>
+    </div>
+  );
+};
 
 export default ProductPage;
