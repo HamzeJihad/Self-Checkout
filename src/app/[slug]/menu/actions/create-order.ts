269853146295e3722e3c89ec5cf4a41ed5e43d70
hamze <hamze@bodyfitteam.com.br>
2025-10-05 22:17:41 -0300

    ensure user is redirected with cpf after order

--- src/app/[slug]/menu/actions/create-order.ts
+++ src/app/[slug]/menu/actions/create-order.ts
@@ -60,6 +60,6 @@ export const createOrder = async (data: CreateOrderInput) => {
             customerCpf: removeCpfPunctuation(data.customerCpf),
         },
     });
-    redirect(`/${data.slug}/orders`);
+    redirect(`/${data.slug}/orders?cpf=${removeCpfPunctuation(data.customerCpf)}`);
 }
 
